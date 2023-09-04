98032ebdb571ca2e95361c42eb0afa9963262f26
Jetty Lee <jettylee@live.cn>
2023-09-04 11:39:59 +0800

    update

--- web/i18n/lang/app-overview.en.ts
+++ web/i18n/lang/app-overview.en.ts
@@ -25,7 +25,7 @@ const translation = {
     callTimes: 'Call times',
     usedToken: 'Used token',
     setAPIBtn: 'Go to setup model provider',
-    tryCloud: 'Or try the cloud version of Dify with free quote',
+    tryCloud: 'Or try the cloud version of FoxAI with free quote',
   },
   overview: {
     title: 'Overview',
@@ -49,7 +49,7 @@ const translation = {
           copyRightPlaceholder: 'Enter the name of the author or organization',
           privacyPolicy: 'Privacy Policy',
           privacyPolicyPlaceholder: 'Enter the privacy policy',
-          privacyPolicyTip: 'Helps visitors understand the data the application collects, see Dify\'s <privacyPolicyLink>Privacy Policy</privacyPolicyLink>.',
+          privacyPolicyTip: 'Helps visitors understand the data the application collects, see FoxAI\'s <privacyPolicyLink>Privacy Policy</privacyPolicyLink>.',
         },
       },
       embedded: {
@@ -67,10 +67,10 @@ const translation = {
         title: 'Customize AI WebApp',
         explanation: 'You can customize the frontend of the Web App to fit your scenario and style needs.',
         way1: {
-          name: 'Fork the client code, modify it and deploy to Vercel (recommended)',
-          step1: 'Fork the client code and modify it',
-          step1Tip: 'Click here to fork the source code into your GitHub account and modify the code',
-          step1Operation: 'Dify-WebClient',
+          name: 'Fork the client code, moFoxAI it and deploy to Vercel (recommended)',
+          step1: 'Fork the client code and moFoxAI it',
+          step1Tip: 'Click here to fork the source code into your GitHub account and moFoxAI the code',
+          step1Operation: 'FoxAI-WebClient',
           step2: 'Configure the Web',
           step2Tip: 'Copy the Web API and APP ID,then paste them into the client code config/index.ts',
           step3: 'Deploy to Vercel',
